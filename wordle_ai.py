baa07474fa10bfe2f344f0d7fa05835cd4b434f0
Acherons_ <kamron.cole13@gmail.com>
2022-01-29 22:38:54 -0500

    implement weighted word list, implement a web gui player

--- wordle_ai.py
+++ wordle_ai.py
@@ -1,13 +1,21 @@
 import csv
+import json
 import random
+import time
 import wordle
+import wordle_ai_utils
 
-ALLOWED_FULL = [word.strip() for word in open(wordle.ALLOWED_GUESSES_PATH).readlines()]
+import numpy as np
 
 from alive_progress import alive_bar
+from pynput.keyboard import Key, Controller
+
+ALLOWED_FULL = [word.strip() for word in open(wordle.ALLOWED_GUESSES_PATH).readlines()]
+WORD_WEIGHTS = json.loads(open('./assets/word_weights.json').read())
+
 
 class WordleAI():
-    __slots__ = ['__letters', '__wordle', '__words', '__first_guess', '__next_guess', '__guesses', '__won', '__results', '__result_str', '__yellow_tried']
+    __slots__ = ['__letters', '__wordle', '__words', '__weights', '__first_guess', '__next_guess', '__guesses', '__won', '__results', '__result_str', '__yellow_tried', '__probability_distribution']
 
 
     def __init__(self, wordle: wordle.Wordle, starting_word: str = 'soare') -> None:
@@ -18,7 +26,10 @@ class WordleAI():
         }
 
         self.__wordle = wordle
-        self.__words = [word for word in ALLOWED_FULL]
+        # self.__words = [word for word in ALLOWED_FULL]
+        self.__words, self.__weights = list(zip(*WORD_WEIGHTS.items()))
+        self.__words = list(self.__words)
+        self.__weights = list(self.__weights)
         self.__first_guess = starting_word
         self.__next_guess = self.__first_guess
         self.__guesses = 0
@@ -26,6 +37,7 @@ class WordleAI():
         self.__results = [None for _ in range(4)]
         self.__result_str = ''
         self.__yellow_tried = []
+        self.__probability_distribution = np.array(self.__weights) / sum(self.__weights)
 
 
     def make_guess(self, raw_guess: str) -> wordle.Guess:
@@ -52,26 +64,91 @@ class WordleAI():
 
     def narrow_words(self):
         for letter, index in self.__letters['CORRECT_ALL']:
-            self.__words = [word for word in self.__words if word[index] == letter]
+            new_words = []
+            for word in self.__words:
+                if word[index] == letter:
+                    new_words.append(word)
+                else:
+                    self.__weights.remove(WORD_WEIGHTS[word])
+            self.__words = new_words
         for letter, index in self.__letters['CORRECT_LETTER']:
-            self.__words = [word for word in self.__words if letter in word]
+            new_words = []
+            for word in self.__words:
+                if letter in word:
+                    new_words.append(word)
+                else:
+                    self.__weights.remove(WORD_WEIGHTS[word])
+            self.__words = new_words
         for letter, _ in self.__letters['WRONG']:
-            self.__words = [word for word in self.__words if letter not in word]
+            new_words = []
+            for word in self.__words:
+                if letter not in word:
+                    new_words.append(word)
+                else:
+                    self.__weights.remove(WORD_WEIGHTS[word])
+            self.__words = new_words
         for letter, index in self.__yellow_tried:
-            self.__words = [word for word in self.__words if word[index] != letter]
+            new_words = []
+            for word in self.__words:
+                if word[index] != letter:
+                    new_words.append(word)
+                else:
+                    self.__weights.remove(WORD_WEIGHTS[word])
+            self.__words = new_words
+
+        self.__probability_distribution = np.array(self.__weights) / sum(self.__weights)
 
 
-    def run(self):
+    def type_guess(self, keyboard, guess: str):
+        for letter in guess:
+            keyboard.press(letter)
+            keyboard.release(letter)
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+
+
+    def run_web_gui(self):
+        keyboard = Controller()
         score = 0
+        time.sleep(1)
         while score < 100 and self.__guesses < wordle.MAX_GUESSES:
-            guess = self.make_guess(self.__next_guess)
-            self.read_report(guess)
+            self.type_guess(keyboard, self.__next_guess)
+            time.sleep(3)
+            game_img = wordle_ai_utils.get_screen(wordle_ai_utils.WORDLE_GAME_BOX_1080P)
+            board = wordle_ai_utils.read_img_to_board(game_img)
+            guess = board.get_guesses()[self.__guesses]
             score = guess.get_score()
+            print(guess, score)
+            self.read_report(guess)
             if self.__guesses == 0:
                 self.__results[0] = self.__next_guess
                 self.__results[1] = score
             self.narrow_words()
-            self.__next_guess = random.choice(self.__words)
+            # self.__next_guess = random.choice(self.__words)
+            self.__next_guess = np.random.choice(self.__words, 1, False, self.__probability_distribution)[0]
+            self.__guesses += 1
+        time.sleep(1)
+        if score == 100:
+            self.__won = True
+
+        self.__results[2] = self.__won
+        self.__results[3] = [word for word in self.__words]
+        self.__result_str = f'"{self.__results[0]}","{self.__results[1]}","{self.__results[2]}","{self.__results[3]}"\n'
+
+
+    def run_cli(self):
+        score = 0
+        while score < 100 and self.__guesses < wordle.MAX_GUESSES:
+            print(self.__guesses)
+            guess = self.make_guess(self.__next_guess)
+            score = guess.get_score()
+            self.read_report(guess)
+            if self.__guesses == 0:
+                self.__results[0] = self.__next_guess
+                self.__results[1] = score
+            self.narrow_words()
+            # self.__next_guess = random.choice(self.__words)
+            self.__next_guess = np.random.choice(self.__words, 1, False, self.__probability_distribution)[0]
             self.__guesses += 1
         
         if score == 100:
@@ -113,21 +190,22 @@ def output_file_to_dict(filename):
     return result_dict
 
 
-
-def main():
-    iterations = 25000000
+def data_compiler():
+    iterations = 100000
     iteration = 0
     result_dict = dict()
     score_filename = f'first_quess_results_score.txt'
     winrate_filename = f'first_quess_results_winrate.txt'
+    words, weights = list(zip(*WORD_WEIGHTS.items()))
+    probability_dist = np.array(weights) / sum(weights)
     try:
         with alive_bar(iterations) as bar:
             while iteration < iterations:
                 # print(f'Running iteration {iteration}')
                 game = wordle.Wordle()
                 # print(game.get_answer())
-                bot = WordleAI(game, random.choice(ALLOWED_FULL))
-                bot.run()
+                bot = WordleAI(game, np.random.choice(words, 1, False, probability_dist)[0])
+                bot.run_cli()
                 results = bot.get_result()
                 guess = results[0]
                 score = results[1]
@@ -169,6 +247,18 @@ def main():
     top_10_words_winrate = final_words_winrate[:10]
     for i in range(len(top_10_words_winrate)):
         print(f'    {top_10_words_winrate[i][0]}'.ljust(16) + str(top_10_words_winrate[i][1][3]))
+        
+
+def main():
+    words, weights = list(zip(*WORD_WEIGHTS.items()))
+    probability_dist = np.array(weights) / sum(weights)
+    game = wordle.Wordle()
+    bot = WordleAI(game, np.random.choice(words, 1, False, probability_dist)[0])
+    # bot.run_cli()
+    bot.run_web_gui()
+    # print(game)
+    # print(game.get_answer())
+    print(bot.get_result()[3])
 
 
 if __name__ == '__main__':
