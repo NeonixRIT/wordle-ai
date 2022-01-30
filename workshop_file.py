2a983b3ec04c81dcdaffe7d27aa90a13f89c4014
Acherons_ <kamron.cole13@gmail.com>
2022-01-29 22:34:20 -0500

    add universal file used for testing/workshopping ideas

---
+++ workshop_file.py
@@ -0,0 +1,67 @@
+import wordle
+import numpy as np
+import json
+
+
+def count_letters():
+    count_dict = dict()
+    for word in wordle.ALLOWED_WORDS:
+        for letter in word:
+            if letter not in count_dict:
+                count_dict[letter] = 0
+            count_dict[letter] += 1
+    return count_dict
+
+
+def weighed_results(count_dict):
+    total = len(wordle.ALLOWED_WORDS) * 5
+    weight_dict = dict()
+    for letter in count_dict:
+        num_letters = count_dict[letter]
+        weight_dict[letter] = round((num_letters / total), 4)
+    return weight_dict
+
+
+def word_weight(weighted_dict):
+    word_weights = dict()
+    for word in wordle.ALLOWED_WORDS:
+        word_dict = dict()
+        for letter in word:
+            if letter in word_dict:
+                weight = word_dict[letter][0]
+                n = word_dict[letter][1]
+                new_n = n + 1
+                new_weight = round((weight * (n / (new_n))) + (weight / (new_n)), 5)
+                word_dict[letter] = [new_weight, new_n]
+            else:
+                word_dict[letter] = [weighted_dict[letter], 1]
+        word_weights[word] = 0
+        for key in word_dict:
+            word_weights[word] += word_dict[key][0]
+        word_weights[word] = round(word_weights[word], 4)
+    return word_weights
+
+
+def main():
+    # count_dict = count_letters()
+    # weight_dict = weighed_results(count_dict)
+
+    # word_weights = word_weight(weight_dict)
+    # import json
+    # with open('./assets/word_weights.json', 'w') as file:
+    #     json.dump(word_weights, file)
+    word_weights = None
+    with open('./assets/word_weights.json') as file:
+        word_weights = json.loads(file.read())
+    
+    words, weight = list(zip(*word_weights.items()))
+    random_word = np.random.choice(words, 5, False, np.array(weight) / sum(weight))
+    print(random_word)
+    # full_list = sorted([list for list in word_weights.items()], key= lambda item: item[1], reverse=True)
+    # top_10_list = full_list[:10]
+    # for entry in top_10_list:
+    #     print(entry)
+
+
+if __name__ == '__main__':
+    main()
